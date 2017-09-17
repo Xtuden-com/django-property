95eb0a5f764c68cbb0ea4532cddc581c2f7f2e47
Robert Coster <rob@signalfire.co.uk>
2017-09-17 13:14:51 +0100

    Removed duplicated code for searching for results in alerter

--- homes/alerter.py
+++ homes/alerter.py
@@ -28,43 +28,19 @@ class Alerter(object):
             self.logger.error(ex)
         return False
 
-    def __get_lettings_search(self, criteria):
-        """
-        Get search queryset based on criteria passed
-        :param criteria: dictionary containing criteria for search
-        :return: LettingQuerySet
-        """
-        lettings = Letting.filtered.published().unexpired()
-        lettings = lettings.filter(
+    def __get_search(self, model, criteria):
+        results = model.filtered.published().unexpired()
+        results = results.filter(
             price__range=(
                 criteria.get('min_price',10000),
                 criteria.get('max_price',1000000)
             )
         )
-        lettings = lettings.filter(bedrooms__gte=criteria.get('min_bedrooms',0))
-        lettings = lettings.filter(property_type__slug=criteria.get('property_type','house'))
-        lettings = lettings.filter(location__distance_lte=(self.__get_point(criteria),D(mi=criteria.get('distance',10))))
-        lettings = lettings.order_by('-created_at')[:5]
-        return lettings
-
-    def __get_sales_search(self, criteria):
-        """
-        Get search queryset based on criteria passed
-        :param criteria: dictionary containing criteria for search
-        :return: SaleQuerySet
-        """
-        sales = Sale.filtered.published().unexpired()
-        sales = sales.filter(
-            price__range=(
-                criteria.get('min_price',10000),
-                criteria.get('max_price',1000000)
-            )
-        )
-        sales = sales.filter(bedrooms__gte=criteria.get('min_bedrooms',0))
-        sales = sales.filter(property_type__slug=criteria.get('property_type','house'))
-        sales = sales.filter(location__distance_lte=(self.__get_point(criteria),D(mi=criteria.get('distance',10))))
-        sales = sales.order_by('-created_at')[:5]
-        return sales
+        results = results.filter(bedrooms__gte=criteria.get('min_bedrooms',0))
+        results = results.filter(property_type__slug=criteria.get('property_type','house'))
+        results = results.filter(location__distance_lte=(self.__get_point(criteria),D(mi=criteria.get('distance',10))))
+        results = results.order_by('-created_at')[:5]
+        return results
 
     def __filter_alerts_to_send(self):
         """
@@ -104,9 +80,9 @@ class Alerter(object):
         alerts_to_send = self.__filter_alerts_to_send()
         for key, config in alerts_to_send.iteritems():
             if config['criteria']['search_type'] == 'sales':
-                properties = self.__get_sales_search(config['criteria'])
+                properties = self.__get_search(Sale, config['criteria'])
             else:
-                properties = self.__get_lettings_search(config['criteria'])
+                properties = self.__get_search(Letting, config['criteria'])
             if len(properties) > 0:
                 self.__send(config['recipients'], properties)
             else:
