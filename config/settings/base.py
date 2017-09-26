8b63bb93b481a73a72c583ee96ff31c30f6334e6
Robert Coster <rob@signalfire.co.uk>
2017-09-26 11:28:32 +0100

    CHanges to styles

--- config/settings/base.py
+++ config/settings/base.py
@@ -166,6 +166,7 @@ AWS_STORAGE_BUCKET_NAME = get_env_variable('AWS_STORAGE_BUCKET_NAME')
 AWS_ACCESS_KEY_ID = get_env_variable('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = get_env_variable('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_HOST = get_env_variable('AWS_S3_HOST')
 
 # Static files
 STATICFILES_LOCATION = 'static'
