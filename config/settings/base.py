7445b3b67747928d524773ff490443eb2688d0a8
Robert Coster <rob@signalfire.co.uk>
2017-09-27 09:37:18 +0100

    Changed DEBUG=X to pull whether in debug from environment variable

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # Debug
-DEBUG = True
+DEBUG = get_env_variable('DEBUG')
 
 # Allowed Hosts
 ALLOWED_HOSTS = []
